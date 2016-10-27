d842be7c8397135804f4a4ab5147d5d5e4fcfa8c
0nlyoung7 <0nlyoung7@gmail.com>
2016-10-28 00:11:10 +0900

    Update contacts

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -8,8 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  users:any[] = [];
 
+  constructor(public navCtrl: NavController) {
+  	this.users = [
+  		{"name":"Woody", "message":"This town ain't big enough for the two of us!", "image" :"http://ionicframework.com/dist/preview-app/www/assets/img/avatar-ts-woody.png"},
+  		{"name":"Buzz Lightyear", "message":"My eyeballs could have been sucked from their sockets!", "image" :"http://ionicframework.com/dist/preview-app/www/assets/img/avatar-ts-buzz.png"},
+  		{"name":"Jessie", "message":"Well aren't you just the sweetest space toy I ever did meet!", "image" :"http://ionicframework.com/dist/preview-app/www/assets/img/avatar-ts-jessie.png"},
+  		{"name":"Mr. Potato Head", "message":"You're not turning me into a Mashed Potato.", "image" :"http://ionicframework.com/dist/preview-app/www/assets/img/avatar-ts-potatohead.png"}
+  	];
   }
 
 }
