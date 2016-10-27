bac27eaebd6e2031532885cd3efd889cc98d3f01
0nlyoung7 <0nlyoung7@gmail.com>
2016-10-27 18:01:54 +0900

    Send image after upload

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -23,20 +23,30 @@ export class ChatPage {
 
       // Output the data to the screen that comes in `message` event.
       self.ss.xpush.on( 'message', function(channel, name, data){
-        var msg = decodeURIComponent( data );
-        self.messages.push( {content:msg} );
+        var msg = decodeURIComponent( data.MG );
+        var type = data.TP ? data.TP : '';
+        self.messages.push( {content:msg, sr:data.SR, type:type} );
       });
     });
   }
 
   public send = () => {
     var msg = encodeURIComponent( this.inputMessage );
-    this.ss.xpush.send('channel01', 'message', msg );
+    var data = { 'MG' : msg };
+    this.ss.xpush.send('channel01', 'message', data );
     this.inputMessage = '';
   }
 
   public selectFile = () => {
     let event = new MouseEvent('click', {bubbles: true});
     this.renderer.invokeElementMethod(this.fileInput.nativeElement, 'dispatchEvent', [event]);
+  }
+
+  public onFileChange = ($event, fileValue) => {
+    var self = this;
+    self.ss.xpush.uploadFileInBrowser('channel01', this.fileInput.nativeElement, function(result){
+      var data = { 'MG' : result, TP : 'I' };
+      self.ss.xpush.send('channel01', 'message', data );
+    });
   }
 }
